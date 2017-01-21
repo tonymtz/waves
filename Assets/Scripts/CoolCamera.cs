e8a45831214cd7a0a6fce0afc2ba3420c993924e
TonyMtz <atony.mtz@gmail.com>
2017-01-21 00:02:58 -0700

    draft

---
+++ Assets/Scripts/CoolCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CoolCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private float damping = 1;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(target.transform.position);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 100f;
+
+    [SerializeField]
+    private float rotatingSpeed = 5f;
+
+    private Rigidbody myRigidbody;
+
+    private bool canAction;
+
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float axisHorizontal = Input.GetAxis("Horizontal");
+        float axisVertical = Input.GetAxis("Vertical");
+        bool isActionButton = Input.GetButton("Action");
+
+        Move(axisHorizontal, axisVertical);
+
+        if (isActionButton)
+        {
+            Action();
+        }
+    }
+
+    void Move(float axisHorizontal, float axisVertical)
+    {
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, new Vector3(axisHorizontal, 0f, axisVertical), rotatingSpeed * Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+
+        if (axisHorizontal != 0f)
+        {
+            myRigidbody.velocity = new Vector3(
+                axisHorizontal * movementSpeed * Time.deltaTime,
+                myRigidbody.velocity.y,
+                myRigidbody.velocity.z
+            );
+        }
+
+        if (axisVertical != 0f)
+        {
+            myRigidbody.velocity = new Vector3(
+                myRigidbody.velocity.x,
+                myRigidbody.velocity.y,
+                axisVertical * movementSpeed * Time.deltaTime
+            );
+        }
+    }
+
+    void Action()
+    {
+        canAction = false;
+        Invoke("HideWeapon", 0.5f);
+    }
+
+    private void HideWeapon()
+    {
+        canAction = true;
+    }
+}
